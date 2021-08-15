f6a41f3b9b3f2bf3f15f50949c088dc3ff6bc70b
mtamilarasi589 <67828752+mtamilarasi589@users.noreply.github.com>
2021-08-15 21:26:58 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DisplayComponent } from './display/display.component';
+import { FormComponent } from './form/form.component';
+
+const routes: Routes = [
+
+  { path: ' ', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: FormComponent },
+  { path: 'display', component: DisplayComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
